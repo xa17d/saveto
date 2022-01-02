53f26b344d632982c5510f4fe376eda5d8e31ccd
Daniel Gehrer <daniel.dev@xa1.at>
2022-01-02 18:40:49 +0100

    Create new project

---
+++ app/src/main/java/at/xa1/saveto/MainActivity.kt
@@ -0,0 +1,38 @@
+package at.xa1.saveto
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import at.xa1.saveto.ui.theme.SaveToTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SaveToTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SaveToTheme {
+        Greeting("Android")
+    }
+}
