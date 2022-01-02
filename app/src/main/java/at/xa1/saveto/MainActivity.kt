9b4c86d24edd586acbcaa3a4c54c7c40a896d0cc
Daniel Gehrer <daniel.dev@xa1.at>
2022-01-02 21:05:49 +0100

    Add IntentBox

--- app/src/main/java/at/xa1/saveto/MainActivity.kt
+++ app/src/main/java/at/xa1/saveto/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import at.xa1.saveto.ui.IntentBox
 import at.xa1.saveto.ui.theme.SaveToTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +15,9 @@ class MainActivity : ComponentActivity() {
             SaveToTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    IntentBox(intent = intent)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SaveToTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/at/xa1/saveto/ui/IntentBox.kt
@@ -0,0 +1,40 @@
+package at.xa1.saveto.ui
+
+import android.content.Intent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun IntentBox(intent: Intent) {
+    Column {
+        IntentProperty(name = "action", value = intent.action)
+        IntentProperty(name = "type", value = intent.type)
+        IntentProperty(name = "data", value = intent.dataString)
+        IntentProperty(name = "categories", value = intent.categories?.joinToString())
+
+        val extras = intent.extras
+        if (extras != null) {
+            extras.keySet().forEach { key ->
+                IntentProperty(name = "extra: $key", value = extras.get(key))
+            }
+        } else {
+            IntentProperty(name = "extras", value = null)
+        }
+    }
+}
+
+@Composable
+private fun IntentProperty(name: String, value: Any?) {
+    TextField(
+        value = value?.toString() ?: "null",
+        readOnly = true,
+        enabled = (value != null),
+        onValueChange = {},
+        modifier = Modifier.fillMaxWidth(),
+        label = { Text(name) }
+    )
+}
