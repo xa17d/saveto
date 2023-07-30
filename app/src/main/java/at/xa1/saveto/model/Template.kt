4eb38603f7b158544923a084e53ff61db0c31def
Daniel Gehrer <daniel.dev@xa1.at>
2023-07-30 13:10:02 +0200

    Add model for Template

---
+++ app/src/main/java/at/xa1/saveto/model/Template.kt
@@ -0,0 +1,98 @@
+package at.xa1.saveto.model
+
+import androidx.annotation.StringRes
+import at.xa1.saveto.R
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.UUID
+
+data class Template(
+    val id: TemplateId,
+    val name: String,
+    val suggestedFilename: String,
+    val addExtensionIfMissing: Boolean
+)
+
+@JvmInline
+value class TemplateId(private val value: String) {
+
+    override fun toString(): String = value
+
+    companion object {
+        fun new(): TemplateId = TemplateId(UUID.randomUUID().toString())
+    }
+}
+
+enum class TemplatePlaceholder(
+    val id: String,
+    @StringRes val description: Int,
+    val value: (TemplatePlaceholderContext) -> String
+) {
+    YYYY("YYYY", R.string.templatePlaceHolderDescriptionYYYY, { context ->
+        context.time.format("y")
+    }),
+    YY("YY", R.string.templatePlaceHolderDescriptionYY, { context ->
+        context.time.format("yy")
+    }),
+    MMMM("MMMM", R.string.templatePlaceHolderDescriptionMMMM, { context ->
+        context.time.format("MMMM")
+    }),
+    MMM("MMM", R.string.templatePlaceHolderDescriptionMMM, { context ->
+        context.time.format("MMM")
+    }),
+    MM("MM", R.string.templatePlaceHolderDescriptionMM, { context ->
+        context.time.format("MM")
+    }),
+    M("M", R.string.templatePlaceHolderDescriptionM, { context ->
+        context.time.format("M")
+    }),
+    DD("DD", R.string.templatePlaceHolderDescriptionDD, { context ->
+        context.time.format("dd")
+    }),
+    D("D", R.string.templatePlaceHolderDescriptionD, { context ->
+        context.time.format("d")
+    }),
+    DAY_OF_WEEK("DAY_OF_WEEK", R.string.templatePlaceHolderDescriptionDAY_OF_WEEK, { context ->
+        context.time.format("eee")
+    }),
+    DAY_OF_WEEK_FULL("DAY_OF_WEEK_FULL", R.string.templatePlaceHolderDescriptionDAY_OF_WEEK_FULL, { context ->
+        context.time.format("eeee")
+    }),
+    HH("HH", R.string.templatePlaceHolderDescriptionHH, { context ->
+        context.time.format("HH")
+    }),
+    H("H", R.string.templatePlaceHolderDescriptionH, { context ->
+        context.time.format("H")
+    }),
+    LOWERCASE_HH("hh", R.string.templatePlaceHolderDescriptionhh, { context ->
+        context.time.format("hh")
+    }),
+    LOWERCASE_H("h", R.string.templatePlaceHolderDescriptionh, { context ->
+        context.time.format("h")
+    }),
+    AM_PM("AMPM", R.string.templatePlaceHolderDescriptionAMPM, { context ->
+        context.time.format("a")
+    }),
+    LOWERCASE_MM("mm", R.string.templatePlaceHolderDescriptionmm, { context ->
+        context.time.format("mm")
+    }),
+    LOWERCASE_M("m", R.string.templatePlaceHolderDescriptionm, { context ->
+        context.time.format("m")
+    }),
+    LOWERCASE_SS("ss", R.string.templatePlaceHolderDescriptionss, { context ->
+        context.time.format("ss")
+    }),
+    LOWERCASE_S("s", R.string.templatePlaceHolderDescriptions, { context ->
+        context.time.format("s")
+    }),
+    ORIGINAL_FILENAME("ORIGINAL_FILENAME", R.string.templatePlaceHolderDescriptionORIGINAL_FILENAME, { context ->
+        context.originalFilename
+    })
+}
+
+data class TemplatePlaceholderContext(
+    val time: ZonedDateTime,
+    val originalFilename: String
+)
+
+fun ZonedDateTime.format(pattern: String): String = DateTimeFormatter.ofPattern(pattern).format(this)
---
+++ app/src/test/java/at/xa1/saveto/model/TemplatePlaceholderTest.kt
@@ -0,0 +1,174 @@
+package at.xa1.saveto.model
+
+import org.junit.Test
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import kotlin.test.assertEquals
+
+class TemplatePlaceholderTest {
+
+    private val context = TemplatePlaceholderContext(
+        time = ZonedDateTime.of(2023, 7, 9, 17, 4, 3, 789, ZoneId.of("Europe/Vienna")),
+        originalFilename = "Test Original Filename"
+    )
+
+    @Test
+    fun `YYYY returns 4 digit year`() {
+        assertEquals(
+            "2023",
+            TemplatePlaceholder.YYYY.value(context)
+        )
+    }
+
+    @Test
+    fun `YY returns 2 digit year`() {
+        assertEquals(
+            "23",
+            TemplatePlaceholder.YY.value(context)
+        )
+    }
+
+    @Test
+    fun `MMMM returns month as full name`() {
+        assertEquals(
+            "July",
+            TemplatePlaceholder.MMMM.value(context)
+        )
+    }
+
+    @Test
+    fun `MMM returns month as three letter text`() {
+        assertEquals(
+            "Jul",
+            TemplatePlaceholder.MMM.value(context)
+        )
+    }
+
+    @Test
+    fun `MM returns month as two digit number`() {
+        assertEquals(
+            "07",
+            TemplatePlaceholder.MM.value(context)
+        )
+    }
+
+    @Test
+    fun `M returns month as two digit number`() {
+        assertEquals(
+            "7",
+            TemplatePlaceholder.M.value(context)
+        )
+    }
+
+    @Test
+    fun `DD returns X`() {
+        assertEquals(
+            "09",
+            TemplatePlaceholder.DD.value(context)
+        )
+    }
+
+    @Test
+    fun `D returns X`() {
+        assertEquals(
+            "9",
+            TemplatePlaceholder.D.value(context)
+        )
+    }
+
+    @Test
+    fun `DAY_OF_WEEK returns X`() {
+        assertEquals(
+            "Sun",
+            TemplatePlaceholder.DAY_OF_WEEK.value(context)
+        )
+    }
+
+    @Test
+    fun `DAY_OF_WEEK_FULL returns X`() {
+        assertEquals(
+            "Sunday",
+            TemplatePlaceholder.DAY_OF_WEEK_FULL.value(context)
+        )
+    }
+
+    @Test
+    fun `HH returns X`() {
+        assertEquals(
+            "17",
+            TemplatePlaceholder.HH.value(context)
+        )
+    }
+
+    @Test
+    fun `H returns X`() {
+        assertEquals(
+            "17",
+            TemplatePlaceholder.H.value(context)
+        )
+    }
+
+    @Test
+    fun `hh returns X`() {
+        assertEquals(
+            "05",
+            TemplatePlaceholder.LOWERCASE_HH.value(context)
+        )
+    }
+
+    @Test
+    fun `h returns X`() {
+        assertEquals(
+            "5",
+            TemplatePlaceholder.LOWERCASE_H.value(context)
+        )
+    }
+
+    @Test
+    fun `AMPM returns X`() {
+        assertEquals(
+            "pm",
+            TemplatePlaceholder.AM_PM.value(context)
+        )
+    }
+
+    @Test
+    fun `mm returns X`() {
+        assertEquals(
+            "04",
+            TemplatePlaceholder.LOWERCASE_MM.value(context)
+        )
+    }
+
+    @Test
+    fun `m returns X`() {
+        assertEquals(
+            "4",
+            TemplatePlaceholder.LOWERCASE_M.value(context)
+        )
+    }
+
+    @Test
+    fun `ss returns X`() {
+        assertEquals(
+            "03",
+            TemplatePlaceholder.LOWERCASE_SS.value(context)
+        )
+    }
+
+    @Test
+    fun `s returns X`() {
+        assertEquals(
+            "3",
+            TemplatePlaceholder.LOWERCASE_S.value(context)
+        )
+    }
+
+    @Test
+    fun `ORIGINAL_FILENAME returns X`() {
+        assertEquals(
+            "Test Original Filename",
+            TemplatePlaceholder.ORIGINAL_FILENAME.value(context)
+        )
+    }
+}
